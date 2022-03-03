b7f233a27d7b5da95e73e052d3069acfe8ff3a9e
jeremypenafiel <100175951+jeremypenafiel@users.noreply.github.com>
2022-03-03 15:34:15 +0800

    Add files via upload

---
+++ Lecture1/Assignments/assignment_lec1_c1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("a. In C, lowercase letters are significant.\n"
+           "b. main is where program execution begins.\n"
+           "c. Opening and closing braces enclose program statements in a routine.\n"
+           "d. All program statements must be terminated by a semicolon.");
+
+    return 0;
+}
